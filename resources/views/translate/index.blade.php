32f557a3d6023fac9cb34b66ed2c56e3774da70a
Md Omor Faruk <mdomorfaruk.lu.cse@gmail.com>
2024-01-12 19:04:40 +0600

    Completed setup and a sample translate

---
+++ resources/views/translate/index.blade.php
@@ -0,0 +1,13 @@
+
+    <div id="google_translate_element"></div>
+
+    <!-- Add this script to the head of your HTML document -->
+    <script type="text/javascript">
+      function googleTranslateElementInit() {
+        new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+      }
+    </script>
+
+    <!-- Add this script after the above script -->
+    <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TranslateController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// })->middleware(['verify.shopify'])->name('home');
+
+Route::get('/', [TranslateController::class, 'index'])->middleware(['verify.shopify'])->name('home');
